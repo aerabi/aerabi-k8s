7e7244a7c205c8f1a8cd0ba5df5392805193a91f
Mohammad-Ali A'RÂBI <aerabi@gmx.de>
2022-03-15 19:36:51 +0100

    Add visitor service

---
+++ api/src/atoms/visitor/visitor.service.spec.ts
@@ -0,0 +1,97 @@
+import { Visitor, VisitorService } from './visitor.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { firstValueFrom, tap } from 'rxjs';
+
+function fail(reason = "fail was called in a test.") {
+  throw new Error(reason);
+}
+
+describe('VisitorService', () => {
+  let service: VisitorService;
+
+  const visitorId = '1';
+  const visitor: Visitor = {
+    id: visitorId,
+    name: 'John Doe',
+    email: 'john@doe.com',
+  };
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      providers: [VisitorService],
+    }).compile();
+
+    service = app.get<VisitorService>(VisitorService);
+    await firstValueFrom(service.createVisitor(visitor));
+  });
+
+  describe('getVisitors', () => {
+    it('should return an array of visitors', (done) => {
+      service
+        .getAllVisitors()
+        .pipe(tap((visitors) => expect(visitors).toContain(visitor)))
+        .subscribe({
+          complete: done,
+          error: fail,
+        });
+    });
+  });
+
+  describe('getVisitor', () => {
+    it('should return a visitor', (done) => {
+      service
+        .getVisitor(visitorId)
+        .pipe(tap((visitor) => expect(visitor).toEqual(visitor)))
+        .subscribe({
+          complete: done,
+          error: fail,
+        });
+    });
+  });
+
+  describe('createVisitor', () => {
+    it('should create a visitor', (done) => {
+      const newVisitor: Visitor = {
+        id: '2',
+        name: 'Jane Doe',
+        email: 'jane@doe.com',
+      };
+      service
+        .createVisitor(newVisitor)
+        .pipe(tap((jane) => expect(jane).toEqual(newVisitor)))
+        .subscribe({
+          complete: done,
+          error: fail,
+        });
+    });
+  });
+
+  describe('updateVisitor', () => {
+    it('should update a visitor', (done) => {
+      const updatedVisitor: Visitor = {
+        id: visitorId,
+        name: 'Jane Doe',
+        email: 'jane@doe.com',
+      };
+      service
+        .updateVisitor(updatedVisitor)
+        .pipe(tap((jane) => expect(jane).toEqual(updatedVisitor)))
+        .subscribe({
+          complete: done,
+          error: fail,
+        });
+    });
+  });
+
+  describe('deleteVisitor', () => {
+    it('should delete a visitor', (done) => {
+      service
+        .deleteVisitor(visitorId)
+        .pipe(tap((visitor) => expect(visitor).toEqual(visitor)))
+        .subscribe({
+          complete: done,
+          error: fail,
+        });
+    });
+  });
+});
